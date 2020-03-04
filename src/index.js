6bb43b1ed425416f3e2965eecc01f622f25b8b02
Maksim Diubo <diubomaksim@gmail.com>
2020-03-04 22:55:35 +0300

    feat: solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+  let result = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+
+    let letter = expr.slice(i, i + 10);
+    let morseLetter = [];
+
+    if (letter == '**********') {
+      result.push(' ');
+    } else {
+      for (let j = 0; j < letter.length; j++) {
+        if (letter[j] == 0) {
+          j++
+        }
+        if (letter[j] == 1 && letter[j + 1] == 0) {
+          morseLetter.push('.');
+          j++
+        }
+        if (letter[j] == 1 && letter[j + 1] == 1) {
+          morseLetter.push('-');
+          j++
+        }
+      }
+    }
+    result.push(MORSE_TABLE[morseLetter.join('')]);
+  }
+  return result.join('');
 }
 
 module.exports = {
